5d790749577b49c4857b28572d748efd510f422f
Thomas Sieverding <Thomas@bajix.com>
2021-12-23 21:33:11 -0800

    2 space formatting

--- src/lib.rs
+++ src/lib.rs
@@ -13,39 +13,40 @@ use syn::DeriveInput;
 
 #[proc_macro_derive(RedisJsonValue)]
 pub fn redis_json_value(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    let struct_type = input.ident;
+  let input = parse_macro_input!(input as DeriveInput);
+  let struct_type = input.ident;
 
-    let expanded = quote! {
-        impl redis::ToRedisArgs for &#struct_type {
-            fn write_redis_args<W>(&self, out: &mut W)
-            where
-                W: ?Sized + redis::RedisWrite
-            {
-                        out.write_arg(&serde_json::to_vec(self).expect("json encoding failed"));
-            }
+  let expanded = quote! {
+    impl redis::ToRedisArgs for &#struct_type {
+        fn write_redis_args<W>(&self, out: &mut W)
+        where
+          W: ?Sized + redis::RedisWrite,
+        {
+          out.write_arg(&serde_json::to_vec(self).expect("json encoding failed"));
         }
-
-        impl redis::FromRedisValue for #struct_type {
-            fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
-                match *v {
-                    redis::Value::Data(ref bytes) => Ok(serde_json::from_slice(bytes)
-                                                        .map_err(|err| {
-                                                            (
-                                                                redis::ErrorKind::TypeError,
-                                                                "json deserialize failed",
-                                                                err.to_string(),
-                                                            )
-                                                        })?),
-                    _ => Err((
-                        redis::ErrorKind::TypeError,
-                        "invalid response type for json",
-                    )
-                             .into()),
-                }
-            }
+      }
+      
+      impl redis::FromRedisValue for #struct_type {
+        fn from_redis_value(v: &redis::Value) -> redis::RedisResult<Self> {
+          match *v {
+            redis::Value::Data(ref bytes) => Ok(serde_json::from_slice(bytes).map_err(|err| {
+              (
+                redis::ErrorKind::TypeError,
+                "json deserialize failed",
+                err.to_string(),
+              )
+            })?),
+            _ => Err(
+              (
+                redis::ErrorKind::TypeError,
+                "invalid response type for json",
+              )
+                .into(),
+            ),
+          }
         }
-    };
+      }
+  };
 
-    TokenStream::from(expanded)
+  TokenStream::from(expanded)
 }
